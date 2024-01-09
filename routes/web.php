f12db35a0596c150c877aac1a3d275ac8f23f877
Alexandre Cardoso <xandecar@hotmail.com>
2024-01-09 13:21:32 -0300

    chore: Component without controller

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,12 @@
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
+use App\Livewire\Home;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index']);
+Route::get('/', Home::class);
 Route::get('/post/create', [PostController::class, 'create']);
 Route::get('/post/edit/{post}', [PostController::class, 'edit'])->name('post.edit');
-Route::get('/user/create', [UserController::class, 'create']);
+// Route::get('/user/create', [UserController::class, 'create']);
 Route::get('/posts', [PostController::class, 'index']);
 Route::get('/post/{post}', [PostController::class, 'show'])->name('post.show');
