4c66e080567b8eb07dba66d44df7ea224f9ff809
Alexandre Eduardo Cardoso <alecar2007@gmail.com>
2025-03-03 08:28:28 -0300

    Removed unused routes

--- routes/web.php
+++ routes/web.php
@@ -10,5 +10,3 @@ Route::get('/', Home::class)->name('home');
 Route::get('/posts', Posts::class)->name('posts');
 Route::get('/post/{post}', Post::class)->name('post.show');
 Route::get('/post/edit/{post}', PostEdit::class)->name('post.edit');
-// Route::get('/post/create', [PostController::class, 'create']);
-// // Route::get('/user/create', [UserController::class, 'create']);
